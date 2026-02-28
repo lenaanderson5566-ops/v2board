b135c92a0a4eaa8d6b9e7389ed1538340063831d
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-02-28 18:26:12 +0800

    refactor: rename default theme folder and references to d1

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function (Request $request) {
     }
     $renderParams = [
         'title' => config('v2board.app_name', 'V2Board'),
-        'theme' => config('v2board.frontend_theme', 'default'),
+        'theme' => config('v2board.frontend_theme', 'd1'),
         'version' => config('app.version'),
         'description' => config('v2board.app_description', 'V2Board is best'),
         'logo' => config('v2board.logo')
@@ -22,8 +22,8 @@ Route::get('/', function (Request $request) {
         $themeService->init();
     }
 
-    $renderParams['theme_config'] = config('theme.' . config('v2board.frontend_theme', 'default'));
-    return view('theme::' . config('v2board.frontend_theme', 'default') . '.dashboard', $renderParams);
+    $renderParams['theme_config'] = config('theme.' . config('v2board.frontend_theme', 'd1'));
+    return view('theme::' . config('v2board.frontend_theme', 'd1') . '.dashboard', $renderParams);
 });
 
 $securePath = config('v2board.secure_path', config('v2board.frontend_admin_path', hash('crc32b', config('app.key'))));
